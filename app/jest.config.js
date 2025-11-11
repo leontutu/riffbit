ae5dbc10ca6ca103b6543c192cd9ead8e3c89e34
leontutu <leo.vin.tut@gmail.com>
2025-11-11 16:13:38 +0100

    feat: Add frontend testing setup
    - Add jest-expo to frontend
    - Add react native testing library to frontend
    - Add jest.setup.js with a first mock for SafeAreaView
    - Add a simple test to verify app spins up and can be tested

---
+++ app/jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    preset: "jest-expo",
+    setupFilesAfterEnv: ["@testing-library/jest-native/extend-expect", "<rootDir>/jest.setup.js"],
+    transformIgnorePatterns: [
+        "node_modules/(?!(jest-)?@?react-native|@react-native-community|@react-navigation|expo(nent)?|@expo(nent)?|@expo-google-fonts|react-clone-referenced-element|@unimodules|unimodules|sentry-expo|native-base|react-native-svg)",
+    ],
+    testPathIgnorePatterns: ["/node_modules/", "/android/", "/ios/"],
+};
